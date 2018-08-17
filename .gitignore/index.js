c45058f3cf4d3ee57354a96b39c05167486f7a5c
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2018-08-17 17:57:24 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -66,8 +66,8 @@ bot.on('message', message => {
 
     }
     
-    if (message.content === "Je vous présente <@480019387093483522> !"){
-message.channel.send('Bonjour tout le monde ! :happy:')
+    if (message.startsWith("Je vous présente <@480019387093483522> !")){
+message.channel.send("Bonjour tout le monde ! :happy:")
     }
 	
 if (message.content === prefix + "aide"){
